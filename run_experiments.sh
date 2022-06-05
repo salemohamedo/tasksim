e92c9478e00003e1113d605ed6eb341dd2a6c569
Omar Salemohamed <omar.salemohamed@gmail.com>
2022-06-05 18:25:09 -0400

    Initial commit

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,9 +1,9 @@
 BASE_ARGS="--nmc --freeze-features --skip-eval --save-results"
 
-python main.py $BASE_ARGS --dataset cifar-10 --increment 2 --num-permutations 1
+python main.py $BASE_ARGS --dataset cifar-10 --increment 2 --num-permutations 100
 
-# python main.py $BASE_ARGS --dataset cifar-100 --increment 10 --num-permutations 1
+python main.py $BASE_ARGS --dataset cifar-100 --increment 10 --num-permutations 100
 
-# python main.py $BASE_ARGS --dataset mnist --increment 2 --num-permutations 1
+python main.py $BASE_ARGS --dataset mnist --increment 2 --num-permutations 100
 
-# python main.py $BASE_ARGS --dataset cub200 --increment 10 --num-permutations 1
+python main.py $BASE_ARGS --dataset cub200 --increment 20 --num-permutations 50
