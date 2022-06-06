03cc901509ff71f8f52e18fdfdb06316c428aee4
Omar Salemohamed <omar.salemohamed@gmail.com>
2022-06-06 14:23:54 -0400

    Initial commit

--- experiments/eval_frozen_features.py
+++ experiments/eval_frozen_features.py
@@ -1,5 +1,6 @@
 from utils.eval_utils import get_run_results, process_run_results, load_config
 
+from collections import OrderedDict
 import pandas as pd
 import numpy as np
 
@@ -9,6 +10,9 @@ RESULT_DIRS = [
     ("MNIST", 'results/run_015'),
     ("CUB200", 'results/run_016')
 ]
+
+DATASET_NAMES = OrderedDict(RESULT_DIRS).keys()
+SIM_METRICS = ["Task2Vec"]
 
 sim_results = []
 task_metric_results = []
@@ -23,14 +27,23 @@ for run in RESULT_DIRS:
     sum_results = process_run_results(*run_results)
 
     sim_results += [sum_results.sim_acc_lin, sum_results.sim_fgt_lin, sum_results.sim_acc_nmc]
-    task_metric_result = [dataset, sum_results.acc_lin, sum_results.fgt_lin, sum_results.acc_nmc, sum_results.fgt_nmc]
+    task_metric_result = [sum_results.acc_lin, sum_results.fgt_lin, sum_results.acc_nmc, sum_results.fgt_nmc]
     task_metric_results.append(task_metric_result)
 
-sim_results = pd.DataFrame(sim_results).T
-task_metric_results = pd.DataFrame(task_metric_results)
+sim_results = pd.DataFrame(sim_results, columns=SIM_METRICS).T
+# sim_results.set_index(["SIM_METRICS"])
+sim_results.columns = pd.MultiIndex.from_product(
+    [DATASET_NAMES, ['$Acc_{LC}$', '$Forget_{LC}$', '$Acc_{NMC}$']])
+task_metric_results = pd.DataFrame(
+    task_metric_results, 
+    columns=['$Acc_{LC}$', '$Forget_{LC}$', '$Acc_{NMC}$',  '$Forget_{NMC}$'],
+    index=DATASET_NAMES)
 
-sim_results.to_csv('experiments/results_frozen_features.sim', float_format='%.3f')
-task_metric_results.to_csv('experiments/results_frozen_features.task', float_format='%.2f')
+# sim_results.to_csv('experiments/results_frozen_features.sim', float_format='%.3f')
+sim_results.to_latex('figures/frozen-features-sim-metrics.tex', float_format='%.3f', escape=False, multicolumn=True, multicolumn_format='c')
 
+# task_metric_results.to_csv('experiments/results_frozen_features.task', float_format='%.2f')
+task_metric_results.to_latex(
+    'figures/frozen-features-task-metrics.tex', float_format='%.2f', escape=False)
 print(sim_results)
 print(task_metric_results)
